441585612db1e7658534beeac264c9cd6e3beea8
Antoine Marmelat <antoinemarmelat@gmail.com>
2025-03-18 18:31:13 +0100

    commit

--- PagePanier.php
+++ PagePanier.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,36 +22,13 @@
                 <ul class="menu">
                     <li><a href="PageAccueil.php">Accueil</a></li>
                     <li><a href="PageAccueil2.php">Rechercher</a></li>
-                    <?php if (isset($_SESSION['user'])): ?>
-                        <li><a href="PagePanier.php">Mon panier</a></li>
-                    <?php else: ?>
-                        <li><a href="PageInscription.php">Se connecter</a></li>
-                    <?php endif; ?>
+                    <li><a href="PagePanier.php">Mon panier</a></li>
                     <li><a href="PageProfil.php">Profil</a></li>
                 </ul>
             </nav>
         </header>
     
-        <div class="Page-Accueil-text">
-            <h1>Qui sommes nous</h1>
-            <p >
-                <strong>Movietrip - Voyagez au cœur de vos films préférés ! </strong><br>
-                Envie de traverser l’écran et de vivre une aventure inoubliable ? 
-                Avec Movietrip, explorez des destinations mythiques au coeur de vos films préférés ! 
-            </p>
-            <ul>
-                <li>🎩 Visitez Poudlard et apprenez à manier la magie </li>
-                <li>🦖 Partez en expédition dans un parc rempli de dinosaures </li>
-                <li>🚀 Voyagez vers une galaxie lointaine, très lointaine... </li>
-            </ul>
-            <p >
-                Marchez dans les pas de vos héros, découvrez des lieux iconiques et plongez dans des expériences uniques.
-                Que vous soyez fan de fantasy, de science-fiction ou d’aventure,
-                Movietrip réalise vos rêves de cinéma. 
-                Préparez-vous à une immersion totale et réservez votre voyage dès maintenant !</p>
-            </p>
-            <a class="Page-Accueil-button" href="PageAccueil2.php" >Réserver un séjour</a>
-        </div>
+        
         <footer>
             <ul class="bas-de-page">
                 <li><a href="#">Mentions légales</a></li>
